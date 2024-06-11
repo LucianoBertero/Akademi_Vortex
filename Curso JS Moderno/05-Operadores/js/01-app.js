e4da5765df883e9a8d2353cc53e83b9230df6677
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-06-11 14:27:30 -0300

    Update index.html and remove unused script tags

---
+++ Curso JS Moderno/05-Operadores/js/01-app.js
@@ -0,0 +1,11 @@
+const n1 = 20;
+const n2 = "30";
+const n3 = 40;
+
+//mayor
+console.log(n1 + n2 + n3);
+console.log(n1 > n3);
+console.log(n1 > n2);
+
+//menor
+console.log(n1 < n3);
---
+++ Curso JS Moderno/05-Operadores/js/02-app.js
@@ -0,0 +1,25 @@
+const n1 = 20;
+const n2 = "30";
+const n3 = 20;
+
+//revisar si son iguales
+
+console.log(n1 == n2);
+console.log(n1 == n3);
+
+//comparador stricto
+
+console.log(n1 === n2);
+console.log(n1 === n3);
+
+console.log(typeof n1);
+console.log(typeof n2);
+
+//diferente
+
+const pass1 = "admin";
+const pass2 = "Admin";
+
+console.log(pass1 != pass2);
+console.log(pass1 !== pass2);
+console.log(pass1 !== parseInt(pass2));
