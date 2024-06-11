bb289c9ba1d51e5be5ec39e2c430d9523afb9235
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-06-11 13:57:56 -0300

    chore: Update index.html and remove unused script tags

---
+++ Curso JS Moderno/03-Strings/js/06-app.js
@@ -0,0 +1,9 @@
+const producto = 'Monitor 20" pulgadas';
+
+const texto = "en Promocion".repeat(3);
+console.log(texto);
+console.log(`${producto} ${texto}`);
+
+const actividad = "Estoy aprendiendo JavaScript Moderno";
+console.log(actividad.split(" "));
+console.log(actividad.split("a"));
