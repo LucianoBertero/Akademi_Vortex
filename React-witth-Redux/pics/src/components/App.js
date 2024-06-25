b0eb5d854de64ab3caf3c0cba6c1fcfc62ce331e
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-06-25 16:06:28 -0300

    modulo 7 terminado

--- React-witth-Redux/pics/src/components/App.js
+++ React-witth-Redux/pics/src/components/App.js
@@ -5,17 +5,8 @@ import ImageList from "./ImageList";
 class App extends React.Component {
   state = { images: [] };
 
-  onSearchSubmit = async (term) => {
-    // Aquí deberías realizar la solicitud a una API para buscar imágenes
-    // Por ejemplo, podrías usar axios para hacer una solicitud a Unsplash API
-    // const response = await axios.get('https://api.unsplash.com/search/photos', {
-    //   params: { query: term },
-    //   headers: {
-    //     Authorization: 'Client-ID YOUR_ACCESS_KEY'
-    //   }
-    // });
-    // this.setState({ images: response.data.results });
-    console.log(term); // Por ahora, solo muestra el término de búsqueda en la consola
+  onSearchSubmit = (term) => {
+    console.log(term);
   };
 
   render() {
